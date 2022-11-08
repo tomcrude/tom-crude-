51cae2d4fb6f1e1a53da35cd8b2679564dc3bfc1
tomcrude <113649664+tomcrude@users.noreply.github.com>
2022-11-07 23:04:19 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
     <meta name='description' content='A website where I show my artistic work' />
     <meta name='copyright' content='None' />
     <meta name='robots' content='index,follow, translate' />
+    <link rel="canonical" href="https://tom-crude.vercel.app/" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
   </Head>
